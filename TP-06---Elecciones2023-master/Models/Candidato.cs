9058d2d2bc2f6a1d7fefd17bdc9eb332b84fb8eb
AlexDro71 <129062425+AlexDro71@users.noreply.github.com>
2023-07-31 23:04:57 -0300

    Delete TP-06---Elecciones2023-master directory

--- TP-06---Elecciones2023-master/Models/Candidato.cs
+++
@@ -1,9 +0,0 @@
-public class Candidato{
-   public int idCandidato {get; set;}
-    public int idPartido {get; set;}
-public string Apellido {get; set;}
-public string Nombre {get; set;}
-public DateTime FechaNacimiento  {get; set;}
-public string Foto {get; set;}
-public string Postulacion {get; set;}
-}
